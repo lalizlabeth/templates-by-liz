0ec7621fe6074f73200a61f5fc12513922a41ab9
lalizlabeth <elizabethylin@gmail.com>
2024-01-19 08:47:58 -0500

    website

--- src/pages/job-tracker/index.tsx
+++ src/pages/job-tracker/index.tsx
@@ -11,46 +11,46 @@ export default function Home(props: HomeProps) {
   return (
     <>
       <Head>
-        <title>Elizabeth&apos;s job tracker</title>
+        <title>Elizabeth&apos;s Job Tracker | Notion Template</title>
         <meta
           name="description"
-          content="A job tracker that focuses on your dreams"
+          content="A simple Notion job tracker to help you keep track of your job applications!"
         />
         <meta name="twitter:card" content="summary_large_image" />
         <meta
           name="twitter:url"
-          content="https://www.templatesbyliz.com/portfolio"
+          content="https://www.templatesbyliz.com/job-tracker"
         />
 
         <meta
           name="twitter:title"
-          content="Elizabeth's portfolio guide | UX & product design"
+          content="Elizabeth's Job tracker | Notion Template"
         />
         <meta
           name="twitter:description"
-          content="Everything you need to know about creating your UX or product design portfolio!"
+          content="A simple Notion job tracker to help you keep track of your job applications!"
         />
         <meta name="twitter:site" content="@lalizlabeth"></meta>
         <meta
           name="twitter:image"
-          content="https://www.templatesbyliz.com/og-images/portfolio.png"
+          content="https://www.templatesbyliz.com/og-images/job-tracker.png"
         />
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <meta
           property="og:image"
-          content="https://www.templatesbyliz.com/og-images/portfolio.png"
+          content="https://www.templatesbyliz.com/og-images/job-tracker.png"
         />
         <meta
           property="og:title"
-          content="Elizabeth's portfolio guide | UX & product design"
+          content="Elizabeth's Job tracker | Notion Template"
         />
         <meta
           property="og:description"
-          content="Everything you need to know about creating your UX or product design portfolio!"
+          content="A simple Notion job tracker to help you keep track of your job applications!"
         />
         <meta
           name="keywords"
-          content="notion templates, notion, ux design, product design, portfolio, portfolio guide, elizabeth's portfolio guide, ux portfolio, product design portfolio, case studies, elizabeth lin, lalizlabeth, elizabeth notion templates"
+          content="notion templates, notion, ux design, job tracker, product design, portfolio, portfolio guide, elizabeth's portfolio guide, ux portfolio, product design portfolio, case studies, elizabeth lin, lalizlabeth, elizabeth notion templates"
         />
         <link rel="icon" href="/favicon-general.png" />
         <link
@@ -67,6 +67,7 @@ export default function Home(props: HomeProps) {
               style={{ borderRadius: 10 }}
               autoPlay
               muted
+              loop
             >
               <source
                 src="/job-tracker/job-tracker.mp4"
@@ -75,20 +76,20 @@ export default function Home(props: HomeProps) {
             </video>
             <div className={styles.headerText}>
               <h1 className={styles.headingLarge}>
-                Elizabeth&apos;s job tracker
+                Elizabeth&apos;s Job Tracker ❋ Notion Template
               </h1>
               <p className={styles.bodyText}>
                 This is the Notion job tracker I used when I was searching for a
-                new job ✨! I know there are a bajillion job trackers out there
+                new job! I know there are a bajillion job trackers out there
                 already, but I think this one is a little unique.
               </p>
               <p className={styles.bodyText}>
                 {
-                  "I included a special area to help you figure out what your dream job looks like. How do you figure out if a job is a good fit? What specific aspects are you prioritizing in your next role?"
+                  "I included a ✨ special ✨ area to help you figure out what your dream job looks like. How do you figure out if a job is a good fit? What specific aspects are you prioritizing in your next role?"
                 }
               </p>
               <p className={styles.bodyText}>
-                Good luck on your job search and I hope this template will help
+                Good luck on your job search and I hope this template helps
                 organize some of the chaos during your job search!
               </p>
             </div>
@@ -101,9 +102,8 @@ export default function Home(props: HomeProps) {
           </header>
 
           <section className={styles.footer}>
-            {" "}
             <Link className={styles.link} href="/">
-              Check out my other Notion templates
+              👀 Check out my other Notion templates!
             </Link>
           </section>
         </div>
