178dd52180c37982b0dea17345efa1fba1c66f69
lalizlabeth <elizabethylin@gmail.com>
2024-07-01 21:04:22 -0400

    email

--- src/helpers/emails/ProductDesign.tsx
+++ src/helpers/emails/ProductDesign.tsx
@@ -1,6 +1,6 @@
 import { sendEmail } from "../server/services/email";
 
-function Portfolio() {
+function ProductDesign() {
   return (
     <div>
       <div>Hi!</div>
@@ -11,29 +11,26 @@ function Portfolio() {
         <a href="https://lalizlabeth.notion.site/Elizabeth-s-Declassified-Guide-to-Product-Design-6238679950f24423aea6edd5d10bf25f?pvs=74">
           here
         </a>
-        . Please don’t hesitate to reach out if you have any additional
-        questions or feedback.
+        . Please reach out if you have any additional questions or feedback.
       </div>
       <br />
       <div>Good luck with your design journey!</div>
       <div>Elizabeth</div>
-      <div>
-        <a href="https://twitter.com/lalizlabeth">@lalizlabeth</a>
-      </div>
       <br />
       <div>
-        p.s. If you’re interested, I&apos;m working on a course called The art
-        of visual design. Learn more{" "}
+        p.s. If you&apos;re interested, I&apos;m working on a course called The
+        art of visual design. Learn more{" "}
         <a href="https://www.artofvisualdesign.com/">here</a> ✨
       </div>
     </div>
   );
 }
 
-export async function sendPortfolioEmail(email: string) {
+export async function sendProductDesign(email: string) {
   await sendEmail({
     to: email,
-    subject: "💖 Thanks for downloading Elizabeth's portfolio guide!",
-    element: <Portfolio />,
+    subject:
+      "💖 Thanks for downloading Elizabeth's Declassified Guide to Product Design!",
+    element: <ProductDesign />,
   });
 }
